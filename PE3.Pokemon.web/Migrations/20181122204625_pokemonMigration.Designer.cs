d7424d150dd38d6395fed912a9d3152b15fbef08
Sven Bourree <sven.bourree@skynet.be>
2018-11-22 21:48:16 +0100

    Added pokemonMigration

--- PE3.Pokemon.web/Migrations/20181122204625_pokemonMigration.Designer.cs
+++ PE3.Pokemon.web/Migrations/20181122204625_pokemonMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PE3.Pokemon.web.Data;
 
 namespace PE3.Pokemon.web.Migrations
 {
     [DbContext(typeof(PokemonContext))]
-    partial class PokemonContextModelSnapshot : ModelSnapshot
+    [Migration("20181122204625_pokemonMigration")]
+    partial class pokemonMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
