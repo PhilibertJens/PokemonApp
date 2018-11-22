74e882d1f933f1851319ab32fab3e67927fa6028
Sven Bourree <sven.bourree@skynet.be>
2018-11-22 16:59:36 +0100

    Added seed data and a migration

--- PE3.Pokemon.web/Data/DataSeeder.cs
+++ PE3.Pokemon.web/Data/DataSeeder.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PE3.Pokemon.web.Entities;
 
 namespace PE3.Pokemon.web.Data
 {
@@ -10,7 +11,27 @@ namespace PE3.Pokemon.web.Data
     {
         public static void Seed(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Entities.Type>().HasData(
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Normal" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Fighting" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Flying" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Poison" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Ground" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Rock" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Bug" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Ghost" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Steel" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Fire" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Water" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Grass" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Electric" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Psychic" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Ice" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Dragon" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Dark" },
+                new Entities.Type { Id = Guid.NewGuid(), Name = "Fairy" }
 
+                );
         }
     }
 }
---
+++ PE3.Pokemon.web/Migrations/20181122155443_TypeMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PE3.Pokemon.web.Migrations
+{
+    public partial class TypeMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
