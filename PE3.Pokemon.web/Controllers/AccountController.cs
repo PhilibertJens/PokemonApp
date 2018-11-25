0ea3ead46f8a669f69ff09cbd164168ace042cc3
Jens Philibert <jens.philibert@hotmail.be>
2018-11-25 11:30:24 +0100

    Added Account Controller and Registration form with validation

---
+++ PE3.Pokemon.web/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PE3.Pokemon.web.Models;
+
+namespace PE3.Pokemon.web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Registration()
+        {
+            AccountRegistrationVm vm = new AccountRegistrationVm();
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Registration(AccountRegistrationVm userData)
+        {
+            if (ModelState.IsValid)
+            {
+                return new RedirectToActionResult("Index", "Home", null);
+            }
+            else return View(userData);
+        }
+    }
+}
---
+++ PE3.Pokemon.web/Models/AccountRegistrationVm.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PE3.Pokemon.web.Models
+{
+    public class AccountRegistrationVm
+    {
+        [Required(ErrorMessage ="Please provide your First name")]
+        [Display(Name ="First name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please provide your Last name")]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please provide a Username")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Your Username must be between {1} and 20 characters")]
+        [RegularExpression(@"^[\w\d.]{5,}$", ErrorMessage = "Username cannot contain whitespaces or special characters")]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please provide a Password")]
+        [MinLength(8, ErrorMessage = "Your password should contain atleast {0} characters")]
+        [MaxLength(30, ErrorMessage = "Your password should not be longer {0} characters")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password), ErrorMessage = "The passwords you provided do not match")]
+        [DataType(DataType.Password)]
+        public string RepeatPassword { get; set; }
+
+        [Required(ErrorMessage = "Please provide a valid e-mail address")]
+        [EmailAddress]
+        public string Email { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
