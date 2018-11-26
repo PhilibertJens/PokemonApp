f46aecec5202e383bae608d6e21f8a06e932ded5
Sven Bourree <sven.bourree@skynet.be>
2018-11-26 14:10:58 +0100

    Added homecontroller in admin area

---
+++ PE3.Pokemon.web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PE3.Pokemon.web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string userName = HttpContext.Session.GetString("Username");
+            if (userName != "admin")
+            {
+                return RedirectToAction("Login", "Account", new { area = "" });
+            }
+
+            return View();
+        }
+    }
+}
