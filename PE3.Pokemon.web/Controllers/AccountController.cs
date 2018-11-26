c64423ca3ae0bc22af6c0e26c5ab4ac73945acc2
Sven Bourree <sven.bourree@skynet.be>
2018-11-26 14:29:58 +0100

    Added logic to encapsulate Admin area

--- PE3.Pokemon.web/Controllers/AccountController.cs
+++ PE3.Pokemon.web/Controllers/AccountController.cs
@@ -37,11 +37,12 @@ namespace PE3.Pokemon.web.Controllers
                 
                 if (getUser != null && getUser?.Password == userData.Password)
                 {
-                    if (userData.Username=="admin" && getUser?.Password ==userData.Password)
+                    HttpContext.Session.SetString("Username", getUser.Username);
+                    if (userData.Username == "admin" && getUser?.Password == userData.Password)
                     {
                         return RedirectToAction("Index", "Home", new { area = "Admin" });
                     }
-                    HttpContext.Session.SetString("Username", getUser.Username);
+                    
                     return new RedirectToActionResult("Index", "Home", null);
                 }
                 else
