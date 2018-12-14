332d59bbb1c215973061eb3fe15720488da3d775
Jens Philibert <jens.philibert@hotmail.be>
2018-12-14 17:45:08 +0100

    Replaced Explore Controller Sessions to 1 PokemonData Session

--- PE3.Pokemon.web/Startup.cs
+++ PE3.Pokemon.web/Startup.cs
@@ -35,6 +35,7 @@ namespace PE3.Pokemon.web
 
             services.AddDbContext<PokemonContext>(options => options.UseSqlServer(Configuration.GetConnectionString("PokemonDb")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMemoryCache();
             services.AddSession();
         }
 
