3f3ea9eb4d3abaacae6d7d374230340017cecbab
Jens Philibert <jens.philibert@hotmail.be>
2019-01-28 18:06:54 +0100

    Added SendFirstMessage post method

--- PE3.Pokemon.web/Controllers/ChatController.cs
+++ PE3.Pokemon.web/Controllers/ChatController.cs
@@ -85,6 +85,61 @@ namespace PE3.Pokemon.web.Controllers
             return View(vm);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SendFirstMessage(ChatSendFirstMessageVm vm)
+        {
+            if (ModelState.IsValid)
+            {
+                string userName = HttpContext.Session.GetString("Username");
+                User user = await pokemonContext.Users
+                    .Where(u => u.Username == userName)
+                    .FirstOrDefaultAsync();
+
+                var serializedReceiver = HttpContext.Session.GetString("ReceiverData");
+                User chatReceiver = JsonConvert.DeserializeObject<User>(serializedReceiver);
+
+                var chatName = userName + ", " + chatReceiver.Username;
+                Chat chat = new Chat
+                {
+                    Name = chatName,
+                    CreatorId = user.Id,
+                    CreateDate = DateTime.Now,
+                    LastMessage = vm.Text,
+                    NumberOfUsers = 2,
+                    NumberOfMessages = 2
+                };
+                pokemonContext.Chats.Add(chat);
+                var id = chat.Id;
+
+                UserChat userChat = new UserChat
+                {
+                    Chat = chat,
+                    User = user
+                };
+                pokemonContext.UserChats.Add(userChat);
+
+                UserChat userChat2 = new UserChat
+                {
+                    Chat = chat,
+                    UserId = chatReceiver.Id
+                };
+                pokemonContext.UserChats.Add(userChat2);
+
+                Message message = new Message
+                {
+                    ChatId = id,
+                    Sender = user,
+                    Text = vm.Text,
+                    SendDate = DateTime.Now
+                };
+                pokemonContext.Messages.Add(message);
+                await pokemonContext.SaveChangesAsync();
+                return new RedirectToActionResult("Index", "Chat", null);
+            }
+            return View(vm);
+        }
+
 
     }
 }
